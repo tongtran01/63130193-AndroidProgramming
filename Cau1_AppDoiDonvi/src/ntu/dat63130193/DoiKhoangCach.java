6e7dac89a746baffae605ff3166d0e093d296d43
tongtran01 <tongdat2003nh@gmail.com>
2024-03-25 02:01:44 +0700

    HoanThanhCau1

--- Cau1_AppDoiDonvi/src/ntu/dat63130193/DoiKhoangCach.java
+++ Cau1_AppDoiDonvi/src/ntu/dat63130193/DoiKhoangCach.java
@@ -19,12 +19,12 @@ public class DoiKhoangCach extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_5;
-	private JTextField textField_1;
-	private JTextField textField_4;
+	private JTextField txtInput1;
+	private JTextField txtOutput1;
+	private JTextField txtOutput2;
+	private JTextField txtOutput3;
+	private JTextField txtInput2;
+	private JTextField txtInput3;
 	private JLabel lb1;
 	private JButton btnNewButton_2;
 
@@ -62,15 +62,15 @@ public class DoiKhoangCach extends JFrame {
 		lblNewLabel.setBounds(291, 39, 166, 42);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(165, 122, 133, 55);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtInput1 = new JTextField();
+		txtInput1.setBounds(165, 122, 133, 55);
+		contentPane.add(txtInput1);
+		txtInput1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(407, 122, 122, 55);
-		contentPane.add(textField_2);
+		txtOutput1 = new JTextField();
+		txtOutput1.setColumns(10);
+		txtOutput1.setBounds(407, 122, 122, 55);
+		contentPane.add(txtOutput1);
 		
 		JLabel lblNewLabel_1 = new JLabel("m");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -88,6 +88,41 @@ public class DoiKhoangCach extends JFrame {
 		contentPane.add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("Convert");
+		btnNewButton.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        if (!txtInput1.getText().isEmpty()) {
+		            // Chuyển đổi từ m sang các đơn vị khác
+		            double meter = Double.parseDouble(txtInput1.getText());
+		            double centimeter = meter / 100;
+		            double kilometer = meter * 0.00001;
+		            txtOutput1.setText(String.format("%.2f",meter ));
+		            txtOutput2.setText(String.format("%.2f", centimeter));
+		            txtOutput3.setText(String.format("%.5f", kilometer));
+		        }
+		        
+		        if (!txtInput2.getText().isEmpty()) {
+		            // Chuyển đổi từ cm sang các đơn vị khác
+		            double centimeter = Double.parseDouble(txtInput2.getText());
+		            double meter = centimeter * 0.01;
+		            double kilometer1 = centimeter * 0.001;
+		            txtOutput1.setText(String.format("%.2f", meter));
+		            txtOutput2.setText(String.format("%.2f", centimeter));
+		            txtOutput3.setText(String.format("%.3f", kilometer1));
+		        }
+		        
+		        if (!txtInput3.getText().isEmpty()) {
+		            // Chuyển đổi từ km sang các đơn vị khác
+		            double kilometer = Double.parseDouble(txtInput3.getText());
+		            double meter = kilometer * 1000;
+		            double centimeter = meter * 100;
+		            txtOutput1.setText(String.format("%.2f", centimeter));
+		            txtOutput2.setText(String.format("%.1f", meter));
+		            txtOutput3.setText(String.format("%.2f", kilometer));
+		        }
+		    }
+		});
+
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNewButton.setBounds(96, 394, 133, 42);
 		contentPane.add(btnNewButton);
@@ -105,25 +140,25 @@ public class DoiKhoangCach extends JFrame {
 		btnNewButton_1.setBounds(300, 394, 122, 42);
 		contentPane.add(btnNewButton_1);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(407, 213, 122, 55);
-		contentPane.add(textField_3);
+		txtOutput2 = new JTextField();
+		txtOutput2.setColumns(10);
+		txtOutput2.setBounds(407, 213, 122, 55);
+		contentPane.add(txtOutput2);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(407, 296, 122, 55);
-		contentPane.add(textField_5);
+		txtOutput3 = new JTextField();
+		txtOutput3.setColumns(10);
+		txtOutput3.setBounds(407, 296, 122, 55);
+		contentPane.add(txtOutput3);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(165, 213, 133, 55);
-		contentPane.add(textField_1);
+		txtInput2 = new JTextField();
+		txtInput2.setColumns(10);
+		txtInput2.setBounds(165, 213, 133, 55);
+		contentPane.add(txtInput2);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(165, 296, 134, 55);
-		contentPane.add(textField_4);
+		txtInput3 = new JTextField();
+		txtInput3.setColumns(10);
+		txtInput3.setBounds(165, 296, 134, 55);
+		contentPane.add(txtInput3);
 		
 		lb1 = new JLabel("");
 		Image user = new ImageIcon(this.getClass().getResource("/S2.jpg")).getImage();
@@ -139,12 +174,12 @@ public class DoiKhoangCach extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 
-				 textField.setText("");     // Reset textField
-				    textField_1.setText("");   // Reset textField_1
-				    textField_2.setText("");   // Reset textField_2
-				    textField_3.setText("");   // Reset textField_3
-				    textField_4.setText("");   // Reset textField_4
-				    textField_5.setText("");   // Reset textField_5
+				 txtInput1.setText("");     // Reset textField
+				    txtInput2.setText("");   // Reset textField_1
+				    txtOutput1.setText("");   // Reset textField_2
+				    txtOutput2.setText("");   // Reset textField_3
+				    txtInput3.setText("");   // Reset textField_4
+				    txtOutput3.setText("");   // Reset textField_5
 			}
 		});
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
