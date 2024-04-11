bfa45d494b1831a2b5dc794f17bbfbacc8ee9302
tongtran01 <tongdat2003nh@gmail.com>
2024-04-11 22:55:10 +0700

    Update

--- UsingRecylerView/app/src/main/java/ntu/dat63130193/usingrecylerview/LandScapeAdapter.java
+++ UsingRecylerView/app/src/main/java/ntu/dat63130193/usingrecylerview/LandScapeAdapter.java
@@ -1,28 +1,56 @@
 package ntu.dat63130193.usingrecylerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
 public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> listData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> listData) {
+        this.context = context;
+        this.listData = listData;
+    }
+
     @NonNull
     @Override
     public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem  =   cai_bom.inflate(R.layout.item_land, parent,false);
+        ItemLandHolder viewholderCreate =  new ItemLandHolder(vItem);
+        return viewholderCreate;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        // Lấy đối tượng hiển thị
+        LandScape landScapeHienThi = listData.get(position);
+        // Trích thông tin
+        String caption = landScapeHienThi.getLandCation();
+        String tenAnh = landScapeHienThi.getLandImageFileName();
+        //Đặt vào các trường thông tin
+         holder.tvCaption.setText(caption);
+         // đặt ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenAnh,"mipmap",packageName);
+        holder.ivLandScape.setImageResource(imageID);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listData.size();
     }
 
     class ItemLandHolder extends RecyclerView.ViewHolder{
