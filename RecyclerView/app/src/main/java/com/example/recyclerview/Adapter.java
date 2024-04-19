7a0ec6327f6d1a1d6251789a4735035b2c2024b7
tongtran01 <145564950+tongtran01@users.noreply.github.com>
2024-04-19 22:45:16 +0700

    Xong bài luyện tập thêm

--- RecyclerView/app/src/main/java/com/example/recyclerview/Adapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Adapter.java
@@ -1,27 +1,50 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import java.util.Collections;
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.examViewHolder>{
+    List<exam> list = Collections.emptyList();
+    Context mcontext;
+
+    public Adapter(List<exam> list, Context mcontext) {
+        this.list = list;
+        this.mcontext= mcontext;
+    }
+
+
     @NonNull
     @Override
     public examViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context1 = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context1);
+
+        View imgView = inflater.inflate(R.layout.examcard,parent,false);
+        examViewHolder examViewHolder = new examViewHolder(imgView);
+        return examViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull examViewHolder holder, int position) {
-
+        holder.examTen.setText(list.get(position).ten);
+        holder.examNgay.setText(list.get(position).ngay);
+        holder.examtBao.setText(list.get(position).thongBao);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     final class examViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -39,12 +62,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.examViewHolder>{
             this.examTen = itemView.findViewById(R.id.examName);
             this.examNgay = itemView.findViewById(R.id.examDate);
             this.examtBao = itemView.findViewById(R.id.examMessage);
-            this.view=itemView;
+            this.view = itemView;
         }
 
         @Override
         public void onClick(View v) {
-
+            int clicked = getAdapterPosition();
+            exam exam = list.get(clicked);
+            Toast.makeText(v.getContext(),"Clicked: "+ exam.getTen(),Toast.LENGTH_SHORT).show();
         }
     }
 
--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Adapter adapter;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Exam List");
+        setSupportActionBar(toolbar);
+        //
+        List<exam> list = new ArrayList<exam>();
+        list.add(new exam("First Exam","May 23, 2015","Best Of Luck"));
+        list.add(new exam("Second Exam","June 09, 2015","b of l"));
+        list.add(new exam("My Test Exam","April 27, 2017","This is testing exam .."));
+        //
+        recycler = findViewById(R.id.recycler);
+        adapter = new Adapter(list,getApplication());
+        //
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
     }
 }
