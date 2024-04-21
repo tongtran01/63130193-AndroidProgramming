b431080abfb3617e3a5d0e6698590d28609d6c0b
tongtran01 <145564950+tongtran01@users.noreply.github.com>
2024-04-21 22:47:43 +0700

    Hoàn thành ví dụ 1

---
+++ Vidu1_Intent/app/src/main/java/dat/tht/vidu1_intent/Activity2.java
@@ -0,0 +1,36 @@
+package dat.tht.vidu1_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        Button button = findViewById(R.id.btnQuayVe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
