949f9f6cde92703950c06c8f7675d0ec5685145e
tongtran01 <145564950+tongtran01@users.noreply.github.com>
2024-04-26 11:07:47 +0700

    Hoàn thành Bottom Navigation Bar

--- BottomNavigation/app/src/main/java/dat/tht/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/dat/tht/bottomnavigation/MainActivity.java
@@ -1,5 +1,6 @@
 package dat.tht.bottomnavigation;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigation.SetOnItemSeletedListener(item -> {
+            switch (item.getItemid()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+
+                    break;
+                case R.id.profile:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new HomeFragment());
+                    break;
+            }
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.commit();
     }
 }
